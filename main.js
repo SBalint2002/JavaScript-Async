d581553fae8664f39395ade702a4def05143e70c
Sárándi <sarandi.balint@gmail.com>
2022-10-25 12:23:56 +0200

    Mind button fix

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
-document.addEventListener('DOMContentLoaded',() => {
-    document.getElementById('mind').addEventListener('click', async () =>{
-        adatBetoltes();
-    });
+document.addEventListener('DOMContentLoaded', () => {
 
-    async function adatBetoltes(){
+    //Mind
+    document.getElementById('mind').addEventListener('click', async () => {
         let response = await fetch('products.json');
         let eredmeny = await response.json();
+        adatMegjelenites(eredmeny.products);
+    });
 
-        //let appleTermekek = eredmeny.products.filter(e => e.brand === 'Apple')
+    //ABC sorrend
+    document.getElementById('abc').addEventListener('click', async () => {
+        adatMegjelenites(termekLista);
+    });
 
-        let termekLista = document.getElementById('termeklista');
+    //Legdrágább
+    document.getElementById('legdragabb').addEventListener('click', async () => {
+        adatMegjelenites(termekLista);
+    });
+    
+    //Leírás
+    document.getElementById('leiras').addEventListener('click', async () => {
+        adatMegjelenites(termekLista);
+    });
+
+    //Ajánlat
+    document.getElementById('ajanlat').addEventListener('click', async () => {
+        adatMegjelenites(termekLista);
+    });
+
+    function adatMegjelenites(termekek){
+        let termekLista = document.getElementById('adatok');
         termekLista.textContent = '';
-        // ha apple kell akkor appleTermekek a forba
-        for (let p of eredmeny.products){
+
+        for (let p of termekek){
             let li = document.createElement('li');
-            li.textContent = p.id + " - " + p.title + " - " + p.description 
-            + " - Price: " + p.price + " - Discount: " + p.discountPercentage 
-            + "% - Rating: " + p.rating + " - Stock: " + p.stock + " - Brand: " + p.brand
-            + " - Category: " + p.category;
+            li.innerHTML = p.id + ". " + p.title + " - " + p.description 
+            + "<br>Price: " + p.price + " - Discount: " + p.discountPercentage 
+            + "%<br>Rating: " + p.rating + " - Stock: " + p.stock + "<br>Brand: " + p.brand
+            + " - Category: " + p.category + "<hr>";
             termekLista.appendChild(li);
         }
     }
 });
+
+//let appleTermekek = eredmeny.products.filter(e => e.brand === 'Apple')
